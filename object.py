c309a1bb06faee4889782e4366d02fce2deb1e04
shokhaway <shoxauey@gmail.com>
2022-07-15 16:32:28 +0500

    aaa

---
+++ object.py
@@ -0,0 +1,29 @@
+#Это классы
+x = 10
+print(type(x))
+
+# math = "salom"
+# print(type(math))
+# print(math.upper())
+
+
+# def salom():
+#     print("Sancho")
+
+# print(type(salom))
+
+class Talaba:
+    def __init__(self,ism,familiya,tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+    def tanishtir(self):
+        return(f"{self.ism} - mening ism. {self.familiya} - mening familiyam")
+# talaba1 = Talaba("olim","Olimov", 2006)
+# talaba2 = Talaba("olim","Olimov", 2006)
+# talaba3 = Talaba("olim","Olimov", 2006)
+# talaba4 = Talaba("olim","Olimov", 2006)
+# talaba5 = Talaba("olim","Olimov", 2006)
+
+# print(talaba1.ism)
+
